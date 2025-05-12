6c9e433da7bb431ec51a2c1bc99313902453d381
galgamex <zhaiyouyun666@gmail.com>
2025-05-12 14:17:57 +0800

    Update ecosystem config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,14 +2,17 @@ module.exports = {
   apps: [
     {
       name: 'kun-galgamex-next',
-      port: 3000,
-      cwd: '.',
+      cwd: '/www/galgamex',
+      script: 'npx',
+      args: 'next start',
       instances: 1,
       autorestart: true,
       watch: false,
       max_memory_restart: '1G',
-      script: 'node_modules/.bin/next',
-      args: 'start'
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3000
+      }
     }
   ]
 }
